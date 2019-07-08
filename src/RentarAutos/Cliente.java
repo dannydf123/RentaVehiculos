c0ea0274f6a8740e4323b219d7365a87c9b6c564
danny diaz <dannydiazf123@gmail.com>
2019-07-08 11:13:39 -0500

    actualizacion 2
    da

---
+++ src/RentarAutos/Cliente.java
@@ -0,0 +1,15 @@
+package RentarAutos;
+
+
+public class Cliente {
+    
+    public String primerNombre;
+    public String primerApellido;
+    public String segundoApellido;
+    public String documento;
+    public String  tipoDocumento;
+    public String nacionalidad;
+    public int edad;
+}
+
+
