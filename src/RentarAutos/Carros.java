9d53d525158539f948fc37e48faa5b6c4e3fc1f5
dannydf123 <52668676+dannydf123@users.noreply.github.com>
2019-07-09 11:28:09 -0500

    vuelta a anterior version
    vuelta

--- src/RentarAutos/Carros.java
+++ src/RentarAutos/Carros.java
@@ -7,11 +7,12 @@ public class Carros {
     public String [] placas = new String[5];
     public String [] modelos = new String [5];
     public double [] precios = new double [5];
-    public boolean [] disponibles = new boolean 
+    public boolean [] disponibles = new boolean[5];
             
             
             
 }
+
 
 
 
